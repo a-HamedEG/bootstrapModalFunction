6021f2fca59b0553ba50fc25deda9240086c0fad
ahmed <a.hamedeg97@gmail.com>
2022-09-01 15:00:41 +0200

    Fixing Conflicts

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let modalBtn = document.getElementById("modalBtn");
+let modalBtnRandom = document.getElementById("modalBtnRandom");
 let modal = document.getElementById("modalMessage")
 let modalBody = document.querySelector(".modal-body");
 let modalBackground = document.querySelector(".modal-content");
@@ -16,7 +17,28 @@ function modalmessage(text, color , timeout, size) {
 
 }
 
-modalBtn.addEventListener("click",function () {
-// Small size is "sm" Large Size is "lg"
-    modalmessage("Hi There","red",750,"sm")
+modalBtn.addEventListener("click",function () {  
+    let inputText = document.getElementById("inputText").value;
+    let inputDuration = document.getElementById("inputDuration").value;
+    let inputColor = document.getElementById("inputColor").value;
+    let inputSize = document.getElementById("inputSize").value;
+    
+    if (inputText == "" || inputDuration == "" || inputColor == "") {
+        modalmessage("Fill the inputs", "Red", 750, "sm")
+    } else {
+        if (inputSize == 1) {
+            inputSize = "sm"
+        } else {
+            inputSize = "lg"
+        }
+    
+        console.log(inputColor,inputDuration,inputSize,inputText)
+        modalmessage(inputText,inputColor,inputDuration,inputSize)
+    }
+
+})
+
+modalBtnRandom.addEventListener("click",function () { 
+    // Small size is "sm" Large Size is "lg"
+    modalmessage("Hi There", "Green", 750, "sm")
 })
